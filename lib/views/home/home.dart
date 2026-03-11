20fc24e71e218efbafe9683eee45cf11401fedb1
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2026-03-11 05:59:40 +0200

    featch all products

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,13 +1,49 @@
 import 'package:cosmetics/core/common/widgets/app_images.dart';
 import 'package:cosmetics/core/common/widgets/app_input.dart';
 import 'package:cosmetics/core/helpers/extensions.dart';
+import 'package:cosmetics/core/network/dio_helper.dart';
 import 'package:cosmetics/core/theme/app_colors/light_app_colors.dart';
 import 'package:cosmetics/core/theme/app_texts/app_text_styles.dart';
 import 'package:cosmetics/core/utils/common_imports.dart';
 import 'package:cosmetics/views/home/widgets/top_rated_product_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Product> topRatedProducts = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTopRatedProducts();
+  }
+
+  Future<void> fetchTopRatedProducts() async {
+    try {
+      final response = await DioHelper.get("/api/Products");
+      final List data = response.data;
+      setState(() {
+        topRatedProducts = data
+            .map((e) => Product.fromJson(e))
+            .take(6)
+            .toList();
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error loading products")));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +62,6 @@ class HomePage extends StatelessWidget {
                 child: Center(child: AppImages(imagePath: '/search.svg')),
               ),
             ),
-
             12.h.ph,
             AppImages(imagePath: '/poster.png'),
             12.h.ph,
@@ -37,28 +72,68 @@ class HomePage extends StatelessWidget {
               ),
             ),
             14.h.ph,
-            GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 6,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 12.w,
-                mainAxisSpacing: 12.h,
-                childAspectRatio: 0.7,
+            if (isLoading)
+              SizedBox(
+                height: 300.h,
+                child: Center(child: CircularProgressIndicator()),
+              )
+            else if (topRatedProducts.isEmpty)
+              Center(child: Text('No products available'))
+            else
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: topRatedProducts.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 12.w,
+                  mainAxisSpacing: 12.h,
+                  childAspectRatio: 0.7,
+                ),
+                itemBuilder: (context, index) {
+                  final product = topRatedProducts[index];
+                  return TopRatedProductCard(
+                    imageUrl: product.imageUrl,
+                    title: product.name,
+                    price: "\$${product.price}",
+                  );
+                },
               ),
-              itemBuilder: (context, index) {
-                return TopRatedProductCard(
-                  imageUrl:
-                      'https://i.pinimg.com/originals/11/f5/22/11f522c7f8ead5519a4b102723f0a89c.jpg',
-                  title: "Face tint / lip tint",
-                  price: "\$120",
-                );
-              },
-            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+  final int stock;
+  final String imageUrl;
+  final int? categoryId;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.stock,
+    required this.imageUrl,
+    this.categoryId,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json["id"],
+      name: json["name"],
+      description: json["description"] ?? "",
+      price: (json["price"] as num).toDouble(),
+      stock: json["stock"],
+      imageUrl: json["imageUrl"],
+      categoryId: json["categoryId"],
     );
   }
 }
